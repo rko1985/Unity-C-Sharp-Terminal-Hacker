6a9ccbe4dbd8be21c1953675e5d1239eb948d061
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-10-30 07:29:07 -1000

    creating win screen

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -90,12 +90,49 @@ public class Hacker : MonoBehaviour
     {
         if(input == password)
         {
-            Terminal.WriteLine("WELL DONE!");
+            DisplayWinScreen();
         }
         else
         {
             Terminal.WriteLine("Sorry, wrong password!");
         }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a book...");
+                Terminal.WriteLine(@"
+    ______
+   /     //
+  /     //
+ /_____//
+(_____(/
+" 
+                );
+                break;
+            case 2:
+                Terminal.WriteLine("You got the prison key!");
+                Terminal.WriteLine(@"
+   __
+  /0 \_______
+  \__/-=' = '
+"
+                );
+                break;
+            default:
+                Debug.LogError("Invalid level reached");
+                break;
+        }        
     }
 }
 
